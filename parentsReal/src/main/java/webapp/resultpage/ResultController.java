37a8650ef4243b1bb977618d085c0036e396abb0
ippokratoys <polydorosthasis@hotmail.com>
2017-05-24 02:16:26 +0300

    Try to create connection search-result page

--- parentsReal/src/main/java/webapp/resultpage/ResultController.java
+++ parentsReal/src/main/java/webapp/resultpage/ResultController.java
@@ -1,15 +1,34 @@
 package webapp.resultpage;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller()
+@Controller
 public class ResultController {
 
-	@RequestMapping(value="/results",method=RequestMethod.POST)
-	public String getResults(){
-		return "results";
-	}
+    @RequestMapping(value="/results",method= RequestMethod.GET)
+    public String search(
+   		@RequestParam Map<String,String> allRequestParams,
+//    	@RequestParam("AboutSearches") String address,
+//   		@RequestParam(name="Date") String date,
+//   		@RequestParam(name="Prices") int price,
+//   		@RequestParam(name="Age") int age,
+//   		@RequestParam(name="Extra_Tags") String extraTags,
+   		Model model)
+    {
+    	System.out.println("The Form elements:");
+    	System.out.println(allRequestParams.toString());
+//    	System.out.println("Address:"+address);
+//    	System.out.println("Date:"+date);
+//    	System.out.println(price);
+//    	System.out.println(age);
+//    	System.out.println("Extra Tags:"+extraTags);
+    	return "search";
+    }
+
 }
--- parentsReal/src/main/java/webapp/searchpage/DefaultSearchPage.java
+++ parentsReal/src/main/java/webapp/searchpage/DefaultSearchPage.java
@@ -4,13 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultSearchPage {
 
     @RequestMapping(value="/search",method= RequestMethod.GET)
-    public String search(Model model){
-    	model.addAttribute("name","ilias");
+    public String search(Model model,
+    	@RequestParam(name="Address_Search") String address,
+   		@RequestParam(name="Date") String date,
+   		@RequestParam(name="Prices") int price,
+   		@RequestParam(name="Age") int age,
+   		@RequestParam(name="Extra_Tags") String extraTags)
+    {
+    	System.out.println("The Form elements:");
+    	System.out.println("Address:"+address);
+    	System.out.println("Date:"+date);
+    	System.out.println(price);
+    	System.out.println(age);
+    	System.out.println("Extra Tags:"+extraTags);
     	return "search";
     }
 
