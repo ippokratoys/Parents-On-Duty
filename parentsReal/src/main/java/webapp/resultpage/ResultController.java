173484a004dde2d3edf2f44bb2d2521a9e72bcc1
ippokratoys <polydorosthasis@hotmail.com>
2017-05-24 03:38:04 +0300

    Converted Search/inex.html page to xhmtl Added needed files to static/templates directory of java project

--- parentsReal/src/main/java/webapp/resultpage/ResultController.java
+++ parentsReal/src/main/java/webapp/resultpage/ResultController.java
@@ -14,21 +14,21 @@ public class ResultController {
     @RequestMapping(value="/results",method= RequestMethod.GET)
     public String search(
    		@RequestParam Map<String,String> allRequestParams,
-//    	@RequestParam("AboutSearches") String address,
-//   		@RequestParam(name="Date") String date,
-//   		@RequestParam(name="Prices") int price,
-//   		@RequestParam(name="Age") int age,
-//   		@RequestParam(name="Extra_Tags") String extraTags,
+    	@RequestParam(name="Address") String address,
+   		@RequestParam(name="Dates") String date,
+   		@RequestParam(name="Price") int price,
+   		@RequestParam(name="Age") int age,
+   		@RequestParam(name="ExtraTags") String extraTags,
    		Model model)
     {
     	System.out.println("The Form elements:");
     	System.out.println(allRequestParams.toString());
-//    	System.out.println("Address:"+address);
-//    	System.out.println("Date:"+date);
-//    	System.out.println(price);
-//    	System.out.println(age);
-//    	System.out.println("Extra Tags:"+extraTags);
-    	return "search";
+    	System.out.println("Address:"+address);
+    	System.out.println("Date:"+date);
+    	System.out.println(price);
+    	System.out.println(age);
+    	System.out.println("Extra Tags:"+extraTags);
+    	return "results";
     }
 
 }
