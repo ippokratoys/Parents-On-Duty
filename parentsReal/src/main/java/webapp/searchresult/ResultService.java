e77e409aaa7d75b2b760ea9afb783a17937e6c81
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-06-22 18:04:59 +0300

    First Implementation of free Text Search. Added the ideal folder to gitignore

--- parentsReal/src/main/java/webapp/searchresult/ResultService.java
+++ parentsReal/src/main/java/webapp/searchresult/ResultService.java
@@ -1,13 +1,26 @@
 package webapp.searchresult;
 
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.search.MultiMatchQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
+import webapp.database.Event;
 import webapp.database.Eventsgroup;
 import webapp.database.elasticsearch.EsEventsgroupRepository;
 import webapp.database.repositories.EventsgroupRepository;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 
 /**
  * Created by thanasis on 22/6/2017.
@@ -24,12 +37,21 @@ public class ResultService {
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
 
-    List<Eventsgroup> getResults(String address){
-        List<Eventsgroup> results=eventsgroupHandler.findByName(address);
-        String allWords[]=address.split(" ");
-        for (String oneWord:allWords) {
-            results.addAll(eventsgroupHandler.findByNameContaining(oneWord));
+    List<Eventsgroup> getResults(String searchterm){
+
+
+        QueryBuilder myQuery= QueryBuilders.multiMatchQuery(searchterm, "name^10", "description^1", "type^2")
+                .fuzziness(Fuzziness.AUTO)
+                .analyzer("standard");
+        Iterable<Eventsgroup> resultsIter= esEventsgroupRepository.search(myQuery);
+        List <Eventsgroup> results=null;
+        ArrayList <Integer> ids=new ArrayList<>();
+        for (Eventsgroup aEventsgroup :
+                resultsIter) {
+            ids.add(aEventsgroup.getIdEventsGroup());
         }
+        results = (List<Eventsgroup>) eventsgroupHandler.findAll(ids);
+
         return results;
     }
 
