7e25f60f23516e6370f50e7c23473ab4af82268a
maridap <mplakia19@gmail.com>
2017-06-24 19:23:03 +0300

    fixed the map from event and result page(you couldn't use it) fixed the host button on register pages (made it bigger)

--- parentsReal/src/main/resources/static/js/map.js
+++ parentsReal/src/main/resources/static/js/map.js
@@ -1,7 +1,8 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
