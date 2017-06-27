341c894323d085ce366e1cbe40fb01f78885c471
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-06-27 03:21:02 +0300

    Starting the filtering search implementation.

--- parentsReal/src/main/java/webapp/services/ResultService.java
+++ parentsReal/src/main/java/webapp/services/ResultService.java
@@ -3,13 +3,11 @@ package webapp.services;
 import org.apache.lucene.queryparser.xml.FilterBuilder;
 import org.apache.lucene.queryparser.xml.builders.RangeFilterBuilder;
 import org.elasticsearch.common.unit.Fuzziness;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.MultiMatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.index.query.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import webapp.database.Customer;
 import webapp.database.Event;
@@ -18,6 +16,8 @@ import webapp.database.elasticsearch.EventSearch;
 import webapp.database.elasticsearch.EventSearchRepository;
 import webapp.database.repositories.EventsgroupRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Filter;
@@ -32,6 +32,11 @@ import static org.elasticsearch.index.query.QueryBuilders.multiMatchQuery;
 @Service
 public class ResultService {
 
+    SimpleDateFormat dateParser = new SimpleDateFormat("dd/mm/yyyy");
+    {
+        SimpleDateFormat dateParser = new SimpleDateFormat("dd/mm/yyyy");
+    }
+
     @Autowired
     EventsgroupRepository eventsgroupHandler;
 
@@ -41,7 +46,7 @@ public class ResultService {
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
 
-    public List<EventSearch> getResults(String searchterm){
+    public List<EventSearch> getResults(String searchterm, String fromDate, String toDate, int price, int age, int distance){
 
         QueryBuilder myQuery= QueryBuilders.multiMatchQuery(searchterm)
                 .field("name^3")
@@ -52,7 +57,28 @@ public class ResultService {
                 .zeroTermsQuery(MatchQueryBuilder.ZeroTermsQuery.ALL)
                 .analyzer("greek");
 
-        Iterable<EventSearch> resultsIter= esEventSearchRepository.search(myQuery);
+
+        RangeQueryBuilder rangeAge = QueryBuilders.rangeQuery("price").from(100).to(1500);
+        RangeQueryBuilder rangeDate = null;
+
+        try {
+            rangeDate = QueryBuilders.rangeQuery("day")
+                    .from( dateParser.parse("1/1/2017") )
+                    .to( dateParser.parse("15/1/2017") );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        BoolQueryBuilder finalQuery = new BoolQueryBuilder()
+                .must(rangeAge)
+                .must(rangeDate);
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(myQuery)
+                .withFilter(finalQuery)
+                .build();
+
+        Iterable<EventSearch> resultsIter = elasticsearchTemplate.queryForList(searchQuery,EventSearch.class);
+
         List <EventSearch> results= new ArrayList<EventSearch>();
         for (EventSearch aEventSearch :
                 resultsIter) {
