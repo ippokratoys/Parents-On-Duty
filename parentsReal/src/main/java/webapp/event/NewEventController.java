163b15be42ff9e22e60e9a3416d1ade5ddf41106
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-06-29 03:45:54 +0300

    Some code lines in order to implement the add_event.

--- parentsReal/src/main/java/webapp/event/NewEventController.java
+++ parentsReal/src/main/java/webapp/event/NewEventController.java
@@ -1,5 +1,6 @@
 package webapp.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import webapp.database.Event;
+import webapp.database.Eventsgroup;
+import webapp.database.repositories.EventRepository;
+import webapp.database.repositories.EventsgroupRepository;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -17,18 +26,45 @@ import java.util.Map;
 public class NewEventController {
 //    the get method is in usercontroller / organiser
 
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    EventsgroupRepository eventsgroupRepository;
+
     @RequestMapping(value = "/organiser/add_event",method = RequestMethod.POST)
     public String newEventFormSubmit(Model model,
                                      @RequestParam Map<String,String> allRequestParams,
                                      @AuthenticationPrincipal final UserDetails userDetails//we add this so we know if is logged to show correct bar
-    ){
+    ) throws ParseException {
         System.out.println("allRequestParams = [" + allRequestParams +"]");
         for (int i = 0; i < allRequestParams.size(); i++) {
             if(allRequestParams.get(i)=="" || allRequestParams.get(i).trim()==""){
                 return "reditect:profile/organiser/add_event?error=empty_field";
             }
         }
+        String dateStr = allRequestParams.get("date");
+        String[] splited = dateStr.split("\\s+");
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(splited[0]);
+        Event newEvent = new Event();
+        //newEvent.setOrganiser();
+        newEvent.setDay(date1);
+        newEvent.setSpots(Integer.parseInt(allRequestParams.get("spots")));
+        newEvent.setTime(Time.valueOf(splited[1]));
+        newEvent.setPrice(Integer.parseInt(allRequestParams.get("price")));
+        newEvent.setImportance(1);
+//        newEvent.setLocation();
 
+        Eventsgroup newEventsgroup = new Eventsgroup();
+        newEventsgroup.setName(allRequestParams.get("title"));
+        newEventsgroup.setDescription(allRequestParams.get("desc"));
+  //      newEventsgroup.setAgeFrom("agefrom");
+//        newEventsgroup.setAgeTo("ageto");
+      //  newEventsgroup.setType("type");
+//        newEventsgroup.setOrganiser();
+//        newEventsgroup.setEvents(newEvent);
+//         newEvent.setEventsgroup(newEventsgroup);
+//        newEventsgroup.setImagePath("the place where we are gonna store");
         //check if form elements are ok
 
         return "redirect:profile/organiser/profile?EventAdded=True";
