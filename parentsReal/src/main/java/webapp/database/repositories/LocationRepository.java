bc8d359292afbc62e0ee4f8d5081fe2a0fb4d676
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-07-01 21:25:19 +0300

    Implemented the accept location admin's feature

--- parentsReal/src/main/java/webapp/database/repositories/LocationRepository.java
+++ parentsReal/src/main/java/webapp/database/repositories/LocationRepository.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Repository;
 
 import webapp.database.*;
 
+import java.util.List;
+
 @Repository
 public interface LocationRepository extends CrudRepository<Location, Integer>{
-
+    List<Location> findAllByCertificateChecked(boolean certificateChecked);
 }
