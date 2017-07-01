b05c7a9ab9cf7062a60447b2cf14faa18cef4bea
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-07-01 19:33:24 +0300

    Adding the New location implementation.

--- parentsReal/src/main/java/webapp/event/NewLocationController.java
+++ parentsReal/src/main/java/webapp/event/NewLocationController.java
@@ -1,5 +1,6 @@
 package webapp.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import webapp.database.Organiser;
+import webapp.database.repositories.LocationRepository;
+import webapp.database.repositories.OrganiserRepository;
+import webapp.services.LocationService;
 
 import java.util.Map;
 
@@ -15,11 +20,32 @@ import java.util.Map;
  */
 @Controller
 public class NewLocationController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @Autowired
+    OrganiserRepository organiserRepository;
+
+    @Autowired
+    private LocationService locationService;
+
     @RequestMapping(value = "/organiser/add_place",method = RequestMethod.POST)
     public String newLocationFormSubmit(Model model,
-                                        @RequestParam Map<String,String> allRequestParams,
+                                        @RequestParam(name="Place") String name,
+                                        @RequestParam(name="route") String route,
+                                        @RequestParam(name="number") String number,
+                                        @RequestParam(name="town") String town,
+                                        @RequestParam(name="lat") String lat,
+                                        @RequestParam(name="lon") String lon,
+                                        @RequestParam(name="zip") String zip,
                                         @AuthenticationPrincipal final UserDetails userDetails//we add this so we know if is logged to show correct bar
     ){
-        return "profile/organiser/my_places";
+        System.out.println("allRequestParams = [" + name + route +"]");
+
+        Organiser organiser= organiserRepository.findOne(userDetails.getUsername());
+        locationService.createLocation(name, route, number, town, lat, lon, zip, organiser);
+
+        return "redirect:/organiser/profile?added_new_location=true";
     }
 }
